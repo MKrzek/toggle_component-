549f32cd832f38d98e71c82e187bdbe4c211060b
Malgorzata <mkrzek@googlemail.com>
2018-03-21 21:51:44 +0000

    create folder structure

---
+++ src/store.js
@@ -0,0 +1,16 @@
+import { createStore, compose, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import createHistory from "history/createBrowserHistory";
+
+import rootReducer from "./reducers/index.js";
+
+export const history = createHistory();
+
+export function configureStore(initialState) {
+  const store = createStore(
+    rootReducer,
+    initialState,
+    compose(applyMiddleware(thunk))
+  );
+  return store;
+}
